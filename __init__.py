7db333b6cd6c1d5076213d9f06bb3a8f14d5c387
Christian S. Perone <christian.perone@gmail.com>
2010-09-27 21:19:50 -0300

    First version avaliable.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 __author__ = "Christian S. Perone <christian.perone@gmail.com>"
+__home__ = "http://pyevolve.sourceforge.net/wordpress"
 __version__ = "0.1"
 
--- markov.py
+++ markov.py
@@ -2,23 +2,25 @@ import pymarksim
 
 from optparse import OptionParser
 from optparse import SUPPRESS_USAGE
-
 import cPickle
 import gzip
 
-import nltk
+from nltk import word_tokenize
 
 def run_train(tokenized, options):
 
     markov_dict = {}
 
     try:
-        file_handle = gzip.open(options.dump_file, "rb")
+        file_handle = gzip.open(options.dump_file, "rb",
+                                compresslevel=options.compress_level)
+        print "File %s found, loading..." % options.dump_file
         markov_dict = cPickle.load(file_handle)
         file_handle.close()
     except Exception:
-        pass
+        print "File %s not found, creating a new one..." % options.dump_file
 
+    print "Updating..."
     for i in xrange(0, len(tokenized)-2):
         key = (tokenized[i], tokenized[i+1])
         value = tokenized[i+2]
@@ -29,47 +31,52 @@ def run_train(tokenized, options):
         else:
             markov_dict[key] = [value]
 
-    file_handle = gzip.open(options.dump_file, "w+b")
+    file_handle = gzip.open(options.dump_file, "w+b",
+                            compresslevel=options.compress_level)
     cPickle.dump(markov_dict, file_handle)
     file_handle.close()
+
+    # Show the chain
+    # for k,v in markov_dict.items():
+    #    print k,v
 
     print "Done !"
 
 def run_check(tokenized, options):
-    file_handle = gzip.open(options.dump_file, "rb")
+    file_handle = gzip.open(options.dump_file, "rb",
+                            compresslevel=options.compress_level)
+    print "File %s found, loading..." % options.dump_file
     markov_dict = cPickle.load(file_handle)
     file_handle.close()
 
     accept_rate = 0.0
 
+    print "Checking..."
+
     for i in xrange(0, len(tokenized)-2):
         key = (tokenized[i], tokenized[i+1])
         value = tokenized[i+2]
 
-        if key in markov_dict:
-            if value in markov_dict[key]:
-                accept_rate += 1.0
-
+        if value in markov_dict.get(key, []):
+            accept_rate += 1.0
+    
     tokenized_len = float(len(tokenized)-2)
-    print "Match %.3f %%" % (accept_rate/tokenized_len)
+    print "Match %.2f %%" % ((accept_rate/tokenized_len)*100.0)
 
 def run_main(options):
     file_handle = open(options.input_text, "r")
     mem_buffer = file_handle.read()
     file_handle.close()
 
-    tokenized = nltk.word_tokenize(mem_buffer)
-        
-    if options.mode == "train":
-        run_train(tokenized, options)
-    
-    if options.mode == "check":
-        run_check(tokenized, options)
+    tokenized = word_tokenize(mem_buffer)
+
+    globals()["run_%s" % options.mode](tokenized, options)
 
 if __name__ == "__main__":
     parser = OptionParser(usage=SUPPRESS_USAGE)
-    print "pymarksim v.%s\nBy %s\n" % (pymarksim.__version__,
-                                       pymarksim.__author__)
+    print "pymarksim v.%s\nBy %s\n%s\n" % (pymarksim.__version__,
+                                           pymarksim.__author__,
+                                           pymarksim.__home__)
     print "Type --help parameter for help.\n"
 
     parser.add_option("-d", "--df", dest="dump_file",
@@ -80,6 +87,9 @@ if __name__ == "__main__":
 
     parser.add_option("-m", "--mode", dest="mode",
                       help="Mode of operation: train, check")
+
+    parser.add_option("-c", "--compress-level", dest="compress_level", default=9, type="int",
+                      help="The gzip compression level, default is 9 (max).")
 
     (options, args) = parser.parse_args()
 
